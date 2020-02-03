e86eaa206c5f67a55e7a6bbebf883682ed02c801
Lachlan Campbell <lachlan@hackclub.com>
2020-02-03 14:32:09 -0500

    Fix testing

--- test/index.js
+++ test/index.js
@@ -1,26 +1,16 @@
-// import test from 'ava'
-// import md from '../dist'
 const test = require('ava')
-const md = require('../dist/index.js')
+const md = require('../dist')
 
 const text = `# Beep
 
-**Boop!** from Hack Club.
+**Boop!** from [Hack Club](https://hackclub.com/).
 `
 
-test('returns html for markdown', t => {
-  // const result = text
-  const result = md(text)
+test('returns html for markdown', async t => {
+  const result = await md(text)
   t.is(typeof result, 'string')
   t.true(result.startsWith('<h1>'))
   t.true(result.includes('<strong>'))
   t.true(result.includes('Hack Club'))
   t.snapshot(result)
 })
-
-// test('returns a gray object for desaturated base colors', t => {
-//   const colors = palx('#444')
-//   t.is(typeof colors, 'object')
-//   t.snapshot(colors)
-//   t.is(Object.keys(colors).length, 4)
-// })
